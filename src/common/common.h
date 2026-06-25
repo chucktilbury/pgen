62f76b989a274d999677a81e61342a81c0d7bef0
Charles Tilbury <ctilbury@protonmail.com>
2026-06-25 10:08:57 -0500

    Initial commit

---
+++ src/common/common.h
@@ -0,0 +1,15 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#include "alloc.h"
+#include "hash.h"
+#include "pointer_list.h"
+// #include "strgs.h"
+#include "string_buffer.h"
+#include "string_list.h"
+#include "errors.h"
+#include "trace.h"
+#include "cmdline.h"
+#include "fileio.h"
+
+#endif /* _COMMON_H_ */
---
+++ src/common/fileio.h
@@ -0,0 +1,8 @@
+
+#ifndef _FILEIO_H_
+#define _FILEIO_H_
+
+const char* find_file(const char* fname, const char* ext);
+
+
+#endif /* _FILEIO_H_ */
